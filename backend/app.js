fec0a534142a84162aa0d258c6518974a0d59f17
macwille <vw.manninen@gmail.com>
2021-01-18 22:15:26 +0200

    update proxy

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,25 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-
+const app = express()
 const productsRouter = require('./controllers/products')
 
-const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 
-app.use('/products', productsRouter)
+app.use('/api/products', productsRouter)
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/*', (req, res) => {
-  console.log('unknown endpoint')
-  res.sendStatus(404)
+app.get('/*', (request, response) => {
+  response.sendFile(path.join(__dirname, './build/index.html'), (error) => {
+    if (error) {
+      response.status(500).send(error)
+    }
+  })
 })
+
+if (process.env.NODE_ENV === 'test') {
+  logger.info('Testing mode detected')
+}
 
 module.exports = app
