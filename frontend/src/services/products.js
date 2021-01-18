bdb3d102ef06044ceed04af06e7ba41677524f6a
macwille <vw.manninen@gmail.com>
2021-01-18 21:18:19 +0200

    Ignore no anonymous default export

--- frontend/src/services/products.js
+++ frontend/src/services/products.js
@@ -4,19 +4,19 @@ import { API_URL } from '../config'
 const baseUrl = API_URL + '/products'
 
 const getBeanies = async () => {
-  const result = await axios.get(baseUrl + '/beanies').then((res) => res.data)
-  return result
+  const response = await axios.get(baseUrl + '/beanies')
+  return response.then((res) => res.data)
 }
 
 const getGloves = async () => {
-  const result = await axios.get(baseUrl + '/gloves').then((res) => res.data)
-  return result
+  const response = await axios.get(baseUrl + '/gloves')
+  return response.then((res) => res.data)
 }
 
 const getMasks = async () => {
-  const result = await axios.get(baseUrl + '/facemasks').then((res) => res.data)
-  return result
+  const response = await axios.get(baseUrl + '/facemasks')
+  return response.then((res) => res.data)
 }
 
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getBeanies, getGloves, getMasks }
